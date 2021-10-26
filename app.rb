70b89b3845f68765954bdacbb57aa1b876ba6083
Rebecca <Beckyrose200@aol.com>
2021-10-26 15:24:10 +0100

    Added index folders for new path types

--- app.rb
+++ app.rb
@@ -2,27 +2,9 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 get '/' do
-  "Hello World"
+  erb(:index)
 end
 
-get '/secret' do 
-  "Pssst...I have a secret :-)"
+get '/koala' do 
+ erb(:index2)
 end
-get '/big_secret' do 
-  "Pssst...I have a big secret :-)"
-end
-
-get '/big_secret/koala' do 
-  #"I love koalas"
-    "<div style = 'border: dashed red'> 
-      <img src = 'https://i.imgur.com/1p5nKyZ.jpeg'>
-    </div>"
-  
-end
-
-get '/secret/bone' do
-  "<div style = 'text-align: center;'>
-  'There is a hidden bone by the oak'
-  </div>"
-end
-
