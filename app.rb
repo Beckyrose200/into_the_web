2b463ee878bb187449407c400e3b119f5c04920f
uburger <ursburger@gmail.com>
2021-10-26 17:15:18 +0200

    adding params

--- app.rb
+++ app.rb
@@ -1,11 +1,22 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
+
 get '/' do
+  'Hola senioras y seniores'
+end
+
+get '/random_koala' do
   @rand_name = ["Amigo", "Misty", "Almond"].sample
   erb(:index)
 end
 
 get '/koala' do 
  erb(:index2)
+end
+
+get '/named_koala' do
+  p params
+  @rand_name = params[:name]
+  erb(:index)
 end
