c86083be550db7224bc5d42f36ca377fc9c82447
Rebecca <Beckyrose200@aol.com>
2021-10-27 15:17:23 +0100

    Changed the get method to a post one

--- app.rb
+++ app.rb
@@ -1,21 +1,17 @@
-require 'sinatra'
+require "sinatra"
 require "sinatra/reloader" if development?
 
-
-get '/' do
-  'Hola senioras y seniores'
+get '/random-cat' do 
+    @name = ["Amigo", "Misty", "Almond"].sample
+    erb(:index)
 end
 
-get '/random_koala' do
-  @rand_name = ["Amigo", "Misty", "Almond"].sample
-  erb(:index)
+post '/named-cat' do
+    p params
+    @name = params[:name]
+    erb(:index)
 end
 
-get '/koala' do 
- erb(:index2)
-end
-
-get '/named_koala' do
-  @rand_name = params[:name]
-  erb(:index)
+get '/cat-form' do
+    erb :cat_form
 end
